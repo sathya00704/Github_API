afc474453443071073fd46af1ee738751fb3e396
Sathya Narayanan S <103351192+sathya00704@users.noreply.github.com>
2024-02-20 17:40:31 +0530

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -17,27 +17,38 @@ class UserProvider with ChangeNotifier {
 
   Future<void> getUserProfile(String username) async {
     final String url = '${Api.api}users/${username}';
+    print(url);
 
     try {
       final response = await http.get(Uri.parse(url), headers: {
         'Authorization': 'token ${Api.token}'
       });
-      final responseData = json.decode(response.body) as Map<String, dynamic>;
 
-      print(responseData['name']);
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        // Successful response
+        final responseData = json.decode(response.body) as Map<String, dynamic>;
 
-      user = User(
-        username: responseData['login'] ?? '',
-        imageUrl: responseData['avatar_url'] ?? '',
-        followers: responseData['followers'] ?? 0,
-        followings: responseData['followings'] ?? 0,
-        public_repo: responseData['public_repos'] ?? 0,
-        joined_data: responseData['created_at'] ?? '',
-      );
+        String? name = responseData['name'];
+        print(name ?? 'Name not provided');
 
-      notifyListeners();
+        user = User(
+          username: responseData['login'] ?? '',
+          imageUrl: responseData['avatar_url'] ?? '',
+          followers: responseData['followers'] ?? 0,
+          followings: responseData['followings'] ?? 0,
+          public_repo: responseData['public_repos'] ?? 0,
+          joined_data: responseData['created_at'] ?? '',
+        );
+
+        notifyListeners();
+      } else {
+        // If response status code is not in the success range
+        print('Request failed with status: ${response.statusCode}');
+      }
     } catch (e) {
-      print(e);
+      // Catching any exceptions
+      print('Error: $e');
     }
+
   }
 }

b6d1534cb32cd8d3d2188ff08cd8a56f3162a3e1
Sathya Narayanan S <103351192+sathya00704@users.noreply.github.com>
2024-02-20 17:41:03 +0530

    Update repo_screen.dart

--- lib/screens/repo_screen.dart
+++ lib/screens/repo_screen.dart
@@ -1,165 +1,67 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_github_api/providers/repo_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class RepoScreen extends StatefulWidget {
-  final String username;
-
-  RepoScreen(this.username);
+class InfoScreen extends StatefulWidget {
+  //const InfoScreen({Key? key}) : super(key: key);
+  String title;
+  String url;
+  InfoScreen(this.title,this.url);
 
   @override
-  State<RepoScreen> createState() => _RepoScreenState();
+  State<InfoScreen> createState() => _InfoScreenState();
 }
 
-class _RepoScreenState extends State<RepoScreen> {
-  var _init = true;
-  var _isLoading = false;
+class _InfoScreenState extends State<InfoScreen> {
+  final _key = UniqueKey();
+  Completer<WebViewController>? _controller;
+  int _position = 1;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_init) {
-      setState(() {
-        _isLoading = true;
-      });
+  void initState() {
+    super.initState();
+    _controller = Completer<WebViewController>();
+  }
 
-      Provider.of<RepoProvider>(context, listen: false)
-          .getRepoList(widget.username)
-          .then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-    _init = false;
+  void doneLoading(String url) {
+    setState(() {
+      _position = 0;
+    });
+  }
+
+  void startLoading(String url) {
+    setState(() {
+      _position = 1;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final repoData = Provider.of<RepoProvider>(context);
-    final f = DateFormat.yMMMMd("en_US");
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.username),
-        backgroundColor: Colors.indigo[900],
-        centerTitle: true,
+        title: Text(widget.title),
+        backgroundColor: Colors.black,
         elevation: 0,
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-        itemCount: repoData.repoList.length,
-        itemBuilder: (ctx, index) => InkWell(
-          onTap: () {},
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 2),
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(10),
-                bottomLeft: Radius.circular(10),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 40,
-                  width: double.infinity,
-                  color: Colors.grey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(repoData.repoList[index].repo_name),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        'Created:',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      Text(f.format(DateTime.parse(repoData
-                          .repoList[index].created_date))),
-                    ],
-                  ),
-                ),
-                if (repoData.repoList[index].description != null)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'Description:',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                        ),
-                        Container(width: 150, child: Text(repoData.repoList[index].description)),
-                      ],
-                    ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        'Last Pushed:',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      Text(f.format(DateTime.parse(
-                          repoData.repoList[index].last_pushed))),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        'Branch:',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      Text(repoData.repoList[index].branch),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 40,
-                  width: double.infinity,
-                  color: Colors.grey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                            'Stars: ${repoData.repoList[index].stars.toString()}'),
-                        Text(repoData.repoList[index].language),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      body: IndexedStack(
+        index: _position,
+        children: <Widget>[
+          WebView(
+            key: _key,
+            initialUrl: widget.url,
+            onWebViewCreated: (WebViewController webViewController) {
+              if (!_controller!.isCompleted) {
+                _controller!.complete(webViewController);
+              }
+            },
+            onPageFinished: doneLoading,
+            onPageStarted: startLoading,
           ),
-        ),
+          Container(
+            color: Colors.white,
+            child: Center(child: CircularProgressIndicator()),
+          ),
+        ],
       ),
     );
   }

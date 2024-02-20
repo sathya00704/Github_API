0f7cb76185a163ee5f26bbc849096cadc4da1fd2
Sathya Narayanan S <103351192+sathya00704@users.noreply.github.com>
2024-02-20 17:39:29 +0530

    Update repo.dart

--- lib/models/repo.dart
+++ lib/models/repo.dart
@@ -1,23 +1,22 @@
-class Repo{
-  final String repo_name;
-  final String created_date;
-  final String last_pushed;
-  final String description;
-  final String branch;
-  final String language;
-  final String url;
-  final int stars;
+class Repo {
+  final String? repo_name;
+  final DateTime? created_date;
+  final String? branch;
+  final String? language;
+  final DateTime? last_pushed;
+  final String? url;
+  final int? stars;
+  final String? description;
 
   Repo({
-    required this.repo_name,
-    required this.created_date,
-    required this.branch,
-    required this.language,
-    required this.last_pushed,
-    required this.description,
-    required this.stars,
-    required this.url,
-
-  });
-
+    this.repo_name,
+    String? created_date,
+    this.branch,
+    this.language,
+    String? last_pushed,
+    this.url,
+    this.stars,
+    this.description,
+  })  : created_date = created_date != null ? DateTime.parse(created_date) : null,
+        last_pushed = last_pushed != null ? DateTime.parse(last_pushed) : null;
 }

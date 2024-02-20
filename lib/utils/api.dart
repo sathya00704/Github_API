267135510883be99fe8b645c3cdeb8294874b994
Sathya Narayanan S <103351192+sathya00704@users.noreply.github.com>
2024-02-20 17:42:05 +0530

    Update api.dart

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class Api{
   static const api='https://api.github.com/';
-  static const token='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';
+  static const token='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';
 }
